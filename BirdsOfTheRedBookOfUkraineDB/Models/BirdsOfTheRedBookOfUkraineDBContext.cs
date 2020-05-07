5c561e0936b921ffb04dc800ca4cf5add4e16e6a
LenaSatory <sanya.d983@gmail.com>
2020-05-07 18:16:09 +0300

    DBConnectionSettings

---
+++ BirdsOfTheRedBookOfUkraineDB/Models/BirdsOfTheRedBookOfUkraineDBContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace BirdsOfTheRedBookOfUkraineDB.Models
+{
+    public class BirdsOfTheRedBookOfUkraineDBContext : DbContext
+    {
+        public virtual DbSet<Orders> Orders { get; set; }
+        public virtual DbSet<Families> Families { get; set; }
+        public virtual DbSet<Genuses> Genuses { get; set; }
+        public virtual DbSet<Species> Species { get; set; }
+        public virtual DbSet<Statuses> Statuses { get; set; }
+        public virtual DbSet<ArealsUA> ArealsUA { get; set; }
+        public virtual DbSet<Regions> Regions { get; set; }
+        public virtual DbSet<BirdPhotos> BirdPhotos { get; set; }
+        public virtual DbSet<Sex> Sex { get; set; }
+
+        public BirdsOfTheRedBookOfUkraineDBContext(DbContextOptions<BirdsOfTheRedBookOfUkraineDBContext> options)
+            :base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
--- BirdsOfTheRedBookOfUkraineDB/Startup.cs
+++ BirdsOfTheRedBookOfUkraineDB/Startup.cs
@@ -8,14 +8,29 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using BirdsOfTheRedBookOfUkraineDB.Models;
+
 namespace BirdsOfTheRedBookOfUkraineDB
 {
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<BirdsOfTheRedBookOfUkraineDBContext>(options => options.UseSqlServer(connection));
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +45,11 @@ namespace BirdsOfTheRedBookOfUkraineDB
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
+                /*endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
-                });
+                });*/
+                endpoints.MapControllers();
             });
         }
     }
